33c6e547c5ae3ba50ecf7f3ee21fdc0d82013c69
Danieljacon <danielcamillo2020@gmail.com>
2022-07-26 20:37:58 -0300

    React Router adicionado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>skdfsdakfasd</h1>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
