72e8c8dd59a6873df8d9c1209fc23f3e95676310
Danieljacon <danielcamillo2020@gmail.com>
2022-07-27 18:34:38 -0300

    Adicionada a requisição para a API.

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,21 +3,25 @@ import movieApi from "../../apis/movieApi";
 import MovieListing from "../MovieListing";
 import { API_KEY } from "../../apis/MovieApiKey";
 import { Container } from "./styles";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 export default function Home() {
+  const movieTitle = "harry potter";
+  const atualPage = 2;
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const movieTitle = "harry potter";
-    const atualPage = 2;
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apikey=${API_KEY}&s=${movieTitle}&type=movie&page=${atualPage}`)
         .catch((error) => {
           console.log(error);
         });
-      console.log(response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
-  }, []);
+  }, [dispatch]);
 
   return (
     <Container>
--- src/components/MovieListing/index.jsx
+++ src/components/MovieListing/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../features/movies/movieSlice";
 
 import { Container } from "./styles";
 
 export default function MovieListing() {
-  return (
-    <Container>
-      asdfasd
-    </Container>
-  )
+  const movies = useSelector(getAllMovies);
+
+  let renderMovies = movies.movies.Response === "True" ? (
+    movies.movies.Search.map((movie) => {
+      return <div key={movie.imdbID}>{movie.Title}</div>;
+    })
+  ) : "No movies found";
+  return <Container>{renderMovies}</Container>;
 }
